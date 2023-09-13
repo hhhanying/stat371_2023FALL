689c9d49508851e8c596dace2504a298b8b6b2d5
Hanying Jiang <hjiang252@wisc.edu>
2023-09-13 13:12:55 -0500

    Initial commit

---
+++ Disc1/code_example1.R
@@ -0,0 +1,29 @@
+# This is a comment.
+# print statement
+print("Hello class!")
+
+# basic arithmetic
+10 + 2 * 3 + 5 ** 3
+
+# create a variable
+a <- 371
+# running the following line will print it out
+a
+
+# create a vector
+b <- c(1, 3, 2, 76, 3)
+# extract its value
+b[2]
+b[c(1,3,5)]
+
+# vector
+1:5
+# seq()
+seq(1, 11, by = 2)
+# rep()
+rep(1:5, 3) 
+c(1:5, rep(1, 3))
+rep(c(2, 3), c(10,2))
+
+# help
+help(hist)
